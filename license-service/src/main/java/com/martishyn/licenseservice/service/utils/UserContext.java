34be96232012cb12a248260ffb9008b3068bf9a2
Andrii Martishyn <contactgently@gmail.com>
2024-06-12 13:48:13 +0300

    Added data from request to ThreadLocal for thread availability check

---
+++ license-service/src/main/java/com/martishyn/licenseservice/service/utils/UserContext.java
@@ -0,0 +1,18 @@
+package com.martishyn.licenseservice.service.utils;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class UserContext {
+    public static final String CORRELATION_ID_HEADER = "tmx-correlation-id";
+    public static final String AUTH_TOKEN_HEADER = "tmx-auth-token";
+    public static final String USER_ID_HEADER = "tmx-user-id";
+    public static final String ORGANIZATION_ID_HEADER = "tmx-organization-id";
+
+    private String correlationId;
+    private String authToken;
+    private String userId;
+    private String organizationId;
+}
---
+++ license-service/src/main/java/com/martishyn/licenseservice/service/utils/UserContextFilter.java
@@ -0,0 +1,27 @@
+package com.martishyn.licenseservice.service.utils;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class UserContextFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        UserContextHolder.getContext().setCorrelationId(httpServletRequest.getHeader(UserContext.CORRELATION_ID_HEADER));
+        UserContextHolder.getContext().setUserId(httpServletRequest.getHeader(UserContext.USER_ID_HEADER));
+        UserContextHolder.getContext().setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN_HEADER));
+        UserContextHolder.getContext().setOrganizationId(httpServletRequest.getHeader(UserContext.ORGANIZATION_ID_HEADER));
+
+        log.info("UserContextFilter Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+
+        filterChain.doFilter(httpServletRequest, servletResponse);
+    }
+}

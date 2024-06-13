b0e04d6ef03a41c8622003ad3a9ac3ad69eb10a1
Andrii Martishyn <contactgently@gmail.com>
2024-06-13 16:23:31 +0300

    Added interceptor for client rest call for resttemplate

--- license-service/src/main/java/com/martishyn/licenseservice/configuration/ServiceConfig.java
+++ license-service/src/main/java/com/martishyn/licenseservice/configuration/ServiceConfig.java
@@ -1,12 +1,18 @@
 package com.martishyn.licenseservice.configuration;
 
+import com.martishyn.licenseservice.service.utils.UserContextInterceptor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @ConfigurationProperties(prefix = "example")
 @Configuration
@@ -19,7 +25,13 @@ public class ServiceConfig {
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        RestTemplate template = new RestTemplate();
+        if (template.getInterceptors().isEmpty()){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            template.getInterceptors().add(new UserContextInterceptor());
+        }
+        return template;
     }
 }
 
